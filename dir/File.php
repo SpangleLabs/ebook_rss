2e1d81e69211681eddb22bd364c1a97cce431d34
joshcoales <joshua@coales.co.uk>
2016-05-14 00:56:38 +0100

    Reorganising File() constructor to only need file name.

--- dir/File.php
+++ dir/File.php
@@ -7,9 +7,9 @@ class File {
     private $fullName;
     private $modTime;
     
-    function __construct($fullName, $modTime) {
+    function __construct($fullName) {
         $this->fullName = $fullName;
-        $this->modTime = $modTime;
+        $this->modTime = filemtime($fullName);
         $this->fileName = substr($fullName, strrpos($fullName, '/') + 1);
     }
 }
