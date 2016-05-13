30dfe5bf2fe011b2b1249732300b4c50cd2263f8
joshcoales <joshua@coales.co.uk>
2016-05-14 00:57:21 +0100

    Adding Directory::loadDirectory() method

--- dir/Directory.php
+++ dir/Directory.php
@@ -13,7 +13,29 @@ class Directory {
         return $this->listItems;
     }
     
-    public static function loadDirectory($dir_name) {
-        //TODO
+    public static function compareModTimes($fileNameA, $fileNameB) {
+        $timeA = filemtime($fileNameA);
+        $timeB = filemtime($fileNameB);
+        return $timeA-$timeB;
+    }
+    
+    public static function loadDirectory($dirName) {
+        $fileNames = array_diff(scandir($directory), array('..', '.'));
+        $fileArray = Array();
+        foreach($fileNames as $fileName) {
+            $fullName = $dirName+"/"+$fileName;
+            $fileArray[] = $fullName;
+        }
+        
+        // Sort the files
+        usort($fileArray, Array("Directory","compareModTimes"));
+        
+        // Create file objects
+        $returnDir = new Directory();
+        foreach($fileArray as $fullName) {
+            $newFile = new File($fullName);
+            $returnDir->addFile($newFile);
+        }
+        return $returnDir;
     }
 }
