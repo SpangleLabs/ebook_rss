1e8944ab01a5169ca6de5ca0d7a10fd795834d55
joshcoales <joshua@coales.co.uk>
2016-05-14 01:06:45 +0100

    Implementing Feed->toXml() function

--- rss/Feed.php
+++ rss/Feed.php
@@ -4,6 +4,8 @@ namespace rss;
 
 class Feed {
     private $itemList = Array();
+    private $desc = "";
+    private $title = "";
     
     public function addItem($newItem) {
         $this->itemList[] = $newItem;
@@ -17,6 +19,16 @@ class Feed {
         $output = '<?xml version="1.0" encoding="UTF-8"?>';
         $output .= '<rss version="2.0">';
         $output .= '<channel>';
+        $output .= '<description>'.$this->desc.'</description>';
+        $output .= '<title>'.$this->title.'</title>';
+        $output .= '<generator>ebook_rss</generator>';
+        $output .= '<link>http://spangle.org.uk/ebook_rss/</link>';
+        foreach($this->listItems() as $item) {
+            $output .= $item->toXml();
+        }
+        $output .= '</channel>';
+        $output .= '</rss>';
+        return $output;
     }
     
     public static function fromDirectory($directory) {
