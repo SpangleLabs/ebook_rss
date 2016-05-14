2934ccbee45057ea5a38174f6d8269d107978c8d
joshcoales <joshua@coales.co.uk>
2016-05-14 01:01:39 +0100

    Adding getters to File class

--- dir/File.php
+++ dir/File.php
@@ -12,4 +12,16 @@ class File {
         $this->modTime = filemtime($fullName);
         $this->fileName = substr($fullName, strrpos($fullName, '/') + 1);
     }
+    
+    function getFileName() {
+        return $this->fileName;
+    }
+    
+    function getFullName() {
+        return $this->fullName;
+    }
+    
+    function getModTime() {
+        return $this->modTime;
+    }
 }
