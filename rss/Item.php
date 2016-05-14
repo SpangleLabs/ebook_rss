5ecc33bd674d8ae437d9073fada651ab520dd9a2
joshcoales <joshua@coales.co.uk>
2016-05-14 01:12:46 +0100

    Implementing Item::fromFile() function

--- rss/Item.php
+++ rss/Item.php
@@ -3,14 +3,45 @@
 namespace rss;
 
 class Item {
-    //TODO
+    private $title = "";
+    private $description = "";
+    private $link = "";
+    private $guid = "";
+    private $pubDate = "";
     
-    function outputTime() {
-        return gmdate("D, j M Y H:i:s O");
+    function setTitle($title) {
+        $this->title = $title;
+    }
+    
+    function setDescription($desc) {
+        $this->description = $desc;
+    }
+    
+    function setLink($link) {
+        $this->link = $link;
+    }
+    
+    function setGuid($guid) {
+        $this->guid = $guid;
+    }
+    
+    function setPubDate($pubDate) {
+        $this->pubDate = $pubDate;
+    }
+    
+    static function outputTime($unixTime) {
+        return gmdate("D, j M Y H:i:s O", $unixTime);
     }
     
     static function fromFile($file) {
-        //TODO
+        $newItem = Item();
+        $newItem->setTitle($file->getFileName());
+        $newItem->setDescription($file->getFullName());
+        $fullLink = "http://spangle.org.uk/ebook_rss/".$file->getFullName();
+        $newItem->setLink($fullLink);
+        $newItem->setGuid($fullLink);
+        $newItem->setPubDate(Item::outputTime($file->getModTime()));
+        return $newItem;
     }
     
     function toXml() {
